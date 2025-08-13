287a2fa076aa8b454de6e5a0bcef6adb44d31dca
Nabin Kandel <nabin.kandel@oic.edu.np>
2025-08-13 21:15:27 +0545

    Update Interfaces

---
+++ ECommerce.Application/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Domain.Models;
+
+namespace ECommerce.Application.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task<Category> AddAsync(Product product);
+        Task UpdateAsync(Category product);
+        Task DeleteAsync(int id);
+    }
+}
--- ECommerce.Domain/Models/Product.cs
+++ ECommerce.Domain/Models/Product.cs
@@ -11,7 +11,10 @@ namespace ECommerce.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
 }
+
